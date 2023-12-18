a89f77124de5124fc0f4a80ce16cf27a3fe3078a
Nizar <145998491+neezr@users.noreply.github.com>
2023-12-18 19:55:11 +0100

    Move main loop into function

--- Auto-Rename MediaIn to file name.lua
+++ Auto-Rename MediaIn to file name.lua
@@ -12,23 +12,27 @@
 -- Install:
 -- Copy this .lua-file into the folder "%appdata%\Blackmagic Design\DaVinci Resolve\Support\Fusion\Scripts\Comp"
 
-comp = fusion:GetCurrentComp()
+function rename_mediain_nodes()
+	comp = fusion:GetCurrentComp()
 
-tools = comp:GetToolList()
+	tools = comp:GetToolList()
 
-for _, tool in ipairs(tools) do
-	if string.find(tool:GetAttrs().TOOLS_Name, "MediaIn") then
-		new_node_name = tool:GetAttrs().TOOLS_Clip_Name
-		
-		if string.find(new_node_name, ".") then --remove file extensions
-			final_dot_index = (new_node_name:reverse()):find("%.")
-			new_node_name = string.sub(new_node_name,1,#new_node_name-final_dot_index)
+	for _, tool in ipairs(tools) do
+		if string.find(tool:GetAttrs().TOOLS_Name, "MediaIn") then
+			new_node_name = tool:GetAttrs().TOOLS_Clip_Name
+			
+			if string.find(new_node_name, ".") then --remove file extensions
+				final_dot_index = (new_node_name:reverse()):find("%.")
+				new_node_name = string.sub(new_node_name,1,#new_node_name-final_dot_index)
+			end
+			
+			if new_node_name:match("^%d+$") then --add prefix to purely numeric file names
+				new_node_name = "_" .. new_node_name
+			end
+			
+			tool:SetAttrs({TOOLS_Name = new_node_name})
 		end
-		
-		if new_node_name:match("^%d+$") then --add prefix to purely numeric file names
-			new_node_name = "_" .. new_node_name
-		end
-		
-		tool:SetAttrs({TOOLS_Name = new_node_name})
 	end
 end
+
+rename_mediain_nodes()
